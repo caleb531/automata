ea0208bd9cef5bb5768d95130565afa7e6ce8704
Caleb Evans <caleb@calebevans.me>
2016-02-16 10:31:53 -0800

    Add docstrings to all NFA tests

--- tests/test_nfa.py
+++ tests/test_nfa.py
@@ -8,8 +8,8 @@ from automata.nfa import NFA
 class TestNFA():
 
     def setup(self):
-        # NFA which matches "a", "aaa", or any string where number of 'a's is
-        # even and greater than zero
+        # NFA which matches "a", "aaa", or any string of 'a's where number of
+        # 'a's is even and greater than zero
         self.nfa = NFA(**{
             'states': {'q0', 'q1', 'q2', 'q3', 'q4',
                        'q5', 'q6', 'q7', 'q8', 'q9'},
@@ -31,44 +31,50 @@ class TestNFA():
         })
 
     def test_validate_automaton_missing_state(self):
+        """should raise error if a state has no transitions defined"""
         with nose.assert_raises(automaton.MissingStateError):
             del self.nfa.transitions['q1']
             self.nfa.validate_automaton()
 
     def test_validate_automaton_invalid_symbol(self):
+        """should raise error if a transition references an invalid symbol"""
         with nose.assert_raises(automaton.InvalidSymbolError):
             self.nfa.transitions['q1']['c'] = {'q2'}
             self.nfa.validate_automaton()
 
     def test_validate_automaton_invalid_state(self):
+        """should raise error if a transition references an invalid state"""
         with nose.assert_raises(automaton.InvalidStateError):
             self.nfa.transitions['q1']['a'] = {'q10'}
             self.nfa.validate_automaton()
 
     def test_validate_automaton_invalid_initial_state(self):
+        """should raise error if the initial state is invalid"""
         with nose.assert_raises(automaton.InvalidStateError):
             self.nfa.initial_state = 'q10'
             self.nfa.validate_automaton()
 
     def test_validate_automaton_invalid_final_state(self):
+        """should raise error if the final state is invalid"""
         with nose.assert_raises(automaton.InvalidStateError):
             self.nfa.final_states = {'q10'}
             self.nfa.validate_automaton()
 
-    def test_validate_input_valid_empty_str_done_reading(self):
-        nose.assert_equal(self.nfa.validate_input('aaa'), {'q4', 'q7', 'q8'})
-
     def test_validate_input_valid(self):
+        """should return correct stop states when valid DFA input is given"""
         nose.assert_equal(
             self.nfa.validate_input('aaaaaa'), {'q5', 'q7', 'q9'})
 
-    def test_validate_input_valid_empty_str_still_reading(self):
-        nose.assert_equal(self.nfa.validate_input('a'), {'q6', 'q8'})
+    def test_validate_input_empty_str(self):
+        """should resolve any empty state transitions on the stop states"""
+        nose.assert_equal(self.nfa.validate_input('aaa'), {'q4', 'q7', 'q8'})
 
     def test_validate_input_invalid_symbol(self):
+        """should raise error if an invalid symbol is read"""
         with nose.assert_raises(automaton.InvalidSymbolError):
             self.nfa.validate_input('aab')
 
     def test_validate_input_nonfinal_state(self):
+        """should raise error if the stop state is not a final state"""
         with nose.assert_raises(automaton.FinalStateError):
             self.nfa.validate_input('aaaaa')
