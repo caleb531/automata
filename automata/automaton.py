d31382c666444c4947ca35bb67ddb851236e2e49
Caleb Evans <caleb@calebevans.me>
2016-02-16 18:49:26 -0800

    Move Automaton class above exception classes

--- automata/automaton.py
+++ automata/automaton.py
@@ -3,6 +3,27 @@
 import abc
 
 
+class Automaton(metaclass=abc.ABCMeta):
+
+    def __init__(self, states, symbols, transitions, initial_state,
+                 final_states):
+        """initialize a complete finite automaton"""
+        self.states = states
+        self.symbols = symbols
+        self.transitions = transitions
+        self.initial_state = initial_state
+        self.final_states = final_states
+        self.validate_automaton()
+
+    @abc.abstractmethod
+    def validate_input(self):
+        pass
+
+    @abc.abstractmethod
+    def validate_automaton(self):
+        pass
+
+
 class AutomatonError(Exception):
     """the base class for all automaton-related errors"""
     pass
@@ -31,24 +52,3 @@ class MissingSymbolError(AutomatonError):
 class FinalStateError(AutomatonError):
     """the automaton stopped at a non-final state"""
     pass
-
-
-class Automaton(metaclass=abc.ABCMeta):
-
-    def __init__(self, states, symbols, transitions, initial_state,
-                 final_states):
-        """initialize a complete finite automaton"""
-        self.states = states
-        self.symbols = symbols
-        self.transitions = transitions
-        self.initial_state = initial_state
-        self.final_states = final_states
-        self.validate_automaton()
-
-    @abc.abstractmethod
-    def validate_input(self):
-        pass
-
-    @abc.abstractmethod
-    def validate_automaton(self):
-        pass
