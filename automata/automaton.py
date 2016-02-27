522b95e12fb18423855123d526d767f1bae704a0
Caleb Evans <caleb@calebevans.me>
2016-02-26 17:17:09 -0800

    Add flake8-docstrings to enforce pep257 convention

--- automata/automaton.py
+++ automata/automaton.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
+"""Classes and methods for working with all finite automata."""
 
 import abc
 
 
 class Automaton(metaclass=abc.ABCMeta):
-    """an abstract base class for finite automata"""
+    """An abstract base class for finite automata."""
 
     @abc.abstractmethod
     def __init__(self, states, symbols, transitions, initial_state,
                  final_states):
-        """initializes a complete finite automaton"""
+        """Initialize a complete finite automaton."""
         pass
 
     @abc.abstractmethod
     def validate_automaton(self):
-        """returns True if this automaton is internally consistent;
-        raises the appropriate exception otherwise"""
+        """Return True if this automaton is internally consistent."""
         pass
 
     def _validate_transition_start_states(self):
-        """raises an error if this automaton's transition start states are
-        missing"""
+        """Raise an error if transition start states are missing."""
         missing_states = self.states - set(self.transitions.keys())
         if missing_states:
             raise MissingStateError(
@@ -28,8 +27,7 @@ class Automaton(metaclass=abc.ABCMeta):
                     ', '.join(missing_states)))
 
     def _validate_transition_end_states(self, path_states):
-        """raises an error if this automaton's transition end states are
-        invalid"""
+        """Raise an error if transition end states are invalid."""
         invalid_states = path_states - self.states
         if invalid_states:
             raise InvalidStateError(
@@ -37,13 +35,13 @@ class Automaton(metaclass=abc.ABCMeta):
                     ', '.join(invalid_states)))
 
     def _validate_initial_state(self):
-        """raises an error if this automaton's initial state is invalid"""
+        """Raise an error if an initial state is invalid."""
         if self.initial_state not in self.states:
             raise InvalidStateError(
                 '{} is not a valid initial state'.format(self.initial_state))
 
     def _validate_final_states(self):
-        """raises an error if this automaton's final states are invalid"""
+        """Raise an error if any final states are invalid."""
         invalid_states = self.final_states - self.states
         if invalid_states:
             raise InvalidStateError(
@@ -52,47 +50,52 @@ class Automaton(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def validate_input(self, input_str):
-        """returns True if the given string is accepted by this automaton;
-        raises the appropriate exception otherwise"""
+        """Check if the given string is accepted by this automaton."""
         pass
 
     def _validate_input_symbol(self, symbol):
-        """raises an error if the given input symbol is invalid"""
+        """Raise an error if the given input symbol is invalid."""
         if symbol not in self.symbols:
             raise InvalidSymbolError(
                 '{} is not a valid input symbol'.format(symbol))
 
     @staticmethod
     def _stringify_states(states):
-        """stringifies the given set of states as a single state name"""
+        """Stringify the given set of states as a single state name."""
         return '{{{}}}'.format(''.join(sorted(states)))
 
 
 class AutomatonError(Exception):
-    """the base class for all automaton-related errors"""
+    """The base class for all automaton-related errors."""
+
     pass
 
 
 class InvalidStateError(AutomatonError):
-    """a state is not a valid state for this automaton"""
+    """A state is not a valid state for this automaton."""
+
     pass
 
 
 class InvalidSymbolError(AutomatonError):
-    """a symbol is not a valid symbol for this automaton"""
+    """A symbol is not a valid symbol for this automaton."""
+
     pass
 
 
 class MissingStateError(AutomatonError):
-    """a state is missing from the transition map"""
+    """A state is missing from the transition map."""
+
     pass
 
 
 class MissingSymbolError(AutomatonError):
-    """a symbol is missing from the transition map"""
+    """A symbol is missing from the transition map."""
+
     pass
 
 
 class FinalStateError(AutomatonError):
-    """the automaton stopped at a non-final state"""
+    """The automaton stopped at a non-final state."""
+
     pass
