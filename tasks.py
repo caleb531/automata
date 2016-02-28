a63ccf863ea33643a45f7666d977a45b71b7edf6
Caleb Evans <caleb@calebevans.me>
2016-02-28 15:12:11 -0800

    Make invoke tasks file comply with PEP8 import order

--- tasks.py
+++ tasks.py
@@ -1,11 +1,14 @@
-# Project tasks (for use with invoke task runner)
+#!/usr/bin/env python3
+"""Project tasks for use with invoke task runner."""
 
 import subprocess
+
 from invoke import task
 
 
 @task
 def test(cover=False):
+    """Run unit tests via nose, and optionally display coverage report also."""
     if cover:
         # Run tests via coverage and generate reports if --cover flag is given
         code = subprocess.call(['coverage', 'run', '-m', 'nose', '--rednose'])
