d7f83b97a2485fe6969d289f5edf8aadb7bd9126
Caleb Evans <caleb@calebevans.me>
2016-05-10 15:20:24 -0700

    Fix misleading compliance test messages

--- tests/test_compliance.py
+++ tests/test_compliance.py
@@ -18,7 +18,6 @@ def test_pep8():
     for file_path in file_paths:
         style_guide = pep8.StyleGuide(quiet=True)
         total_errors = style_guide.input_file(file_path)
-        test_pep8.__doc__ = '{} should comply with PEP 8'.format(file_path)
         fail_msg = '{} does not comply with PEP 8'.format(file_path)
         yield nose.assert_equal, total_errors, 0, fail_msg
 
@@ -32,9 +31,6 @@ def test_complexity():
         with open(file_path, 'r') as file_obj:
             blocks = radon.cc_visit(file_obj.read())
         for block in blocks:
-            test_doc = '{} ({}) should have a low cyclomatic complexity score'
-            test_complexity.__doc__ = test_doc.format(
-                block.name, file_path)
             fail_msg = '{} ({}) has a cyclomatic complexity of {}'.format(
                 block.name, file_path, block.complexity)
             yield nose.assert_less_equal, block.complexity, 10, fail_msg
@@ -48,8 +44,6 @@ def test_import_order():
     for file_path in file_paths:
         with open(file_path, 'r') as file_obj:
             file_contents = file_obj.read()
-        test_doc = '{} imports should comply with PEP 8'
-        test_import_order.__doc__ = test_doc.format(file_path)
         len_change = isort.SortImports(
             file_contents=file_contents).length_change
         fail_msg = '{} imports do not comply with PEP 8'.format(file_path)
