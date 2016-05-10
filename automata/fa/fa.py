386e7e39277cddea8e61091fc5e2eed770a3d8ce
Caleb Evans <caleb@calebevans.me>
2016-05-10 15:39:08 -0700

    Consoldate shared automaton methods

--- automata/fa/fa.py
+++ automata/fa/fa.py
@@ -4,15 +4,11 @@
 import abc
 
 import automata.shared.exceptions as exceptions
+from automata.shared.automaton import Automaton
 
 
-class FA(metaclass=abc.ABCMeta):
+class FA(Automaton, metaclass=abc.ABCMeta):
     """An abstract base class for finite automata."""
-
-    @abc.abstractmethod
-    def __init__(self, **kwargs):
-        """Initialize a complete finite FA."""
-        pass
 
     def _validate_transition_start_states(self):
         """Raise an error if transition start states are missing."""
@@ -30,40 +26,8 @@ class FA(metaclass=abc.ABCMeta):
                 'transition end states are not valid ({})'.format(
                     ', '.join(invalid_states)))
 
-    def _validate_initial_state(self):
-        """Raise an error if an initial state is invalid."""
-        if self.initial_state not in self.states:
-            raise exceptions.InvalidStateError(
-                '{} is not a valid initial state'.format(self.initial_state))
-
-    def _validate_final_states(self):
-        """Raise an error if any final states are invalid."""
-        invalid_states = self.final_states - self.states
-        if invalid_states:
-            raise exceptions.InvalidStateError(
-                'final states are not valid ({})'.format(
-                    ', '.join(invalid_states)))
-
-    @abc.abstractmethod
-    def validate_self(self):
-        """Return True if this FA is internally consistent."""
-        pass
-
     def _validate_input_symbol(self, symbol):
         """Raise an error if the given input symbol is invalid."""
         if symbol not in self.input_symbols:
             raise exceptions.InvalidSymbolError(
                 '{} is not a valid input symbol'.format(symbol))
-
-    @abc.abstractmethod
-    def validate_input(self, input_str):
-        """Check if the given string is accepted by this FA."""
-        pass
-
-    def copy(self):
-        """Create an exact copy of the FA."""
-        return self.__class__(self)
-
-    def __eq__(self, other):
-        """Check if two automata are equal."""
-        return self.__dict__ == other.__dict__
--- automata/tm/tm.py
+++ automata/tm/tm.py
@@ -5,15 +5,11 @@ import abc
 
 import automata.shared.exceptions as exceptions
 import automata.tm.exceptions as tmexceptions
+from automata.shared.automaton import Automaton
 
 
-class TM(metaclass=abc.ABCMeta):
+class TM(Automaton, metaclass=abc.ABCMeta):
     """An abstract base class for Turing machines."""
-
-    @abc.abstractmethod
-    def __init__(self, **kwargs):
-        """Initialize a complete Turing machine."""
-        pass
 
     def _validate_input_symbol_subset(self):
         if not (self.input_symbols < self.tape_symbols):
@@ -22,41 +18,9 @@ class TM(metaclass=abc.ABCMeta):
                 'symbol set ({})'.format(
                     self.tape_symbols - self.input_symbols))
 
-    def _validate_initial_state(self):
-        """Raise an error if an initial state is invalid."""
-        if self.initial_state not in self.states:
-            raise exceptions.InvalidStateError(
-                '{} is not a valid initial state'.format(self.initial_state))
-
-    def _validate_final_states(self):
-        """Raise an error if any final states are invalid."""
-        invalid_states = self.final_states - self.states
-        if invalid_states:
-            raise exceptions.InvalidStateError(
-                'final states are not valid ({})'.format(
-                    ', '.join(invalid_states)))
-
     def _validate_nonfinal_initial_state(self):
         """Raise an error if the initial state is a final state."""
         if self.initial_state in self.final_states:
             raise tmexceptions.InitialStateError(
                 'initial state {} cannot be a final state'.format(
                     self.initial_state))
-
-    @abc.abstractmethod
-    def validate_self(self):
-        """Return True if this machine is internally consistent."""
-        pass
-
-    @abc.abstractmethod
-    def validate_input(self, input_str):
-        """Check if the given string is accepted by this machine."""
-        pass
-
-    def copy(self):
-        """Create an exact copy of the TM."""
-        return self.__class__(self)
-
-    def __eq__(self, other):
-        """Check if two machines are equal."""
-        return self.__dict__ == other.__dict__
