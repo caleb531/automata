7a22aa96989a77b471dcb6135945df03aaaf7aca
Caleb Evans <caleb@calebevans.me>
2016-06-18 19:28:46 -0700

    Add pip setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Setup logic for pip."""
+
+from setuptools import setup
+
+setup(
+    name='automata',
+    version='0.9.0',
+    description='A Python library for simulating automata and Turing machines',
+    url='https://github.com/caleb531/automata',
+    author='Caleb Evans',
+    author_email='caleb@calebevans.me',
+    license='MIT',
+    keywords='automata turing machine',
+    packages=['automata'],
+)
