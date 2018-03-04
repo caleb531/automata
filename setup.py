4f3f5679fab624ed1f08bb4e89f2ed03cc6f98e4
Caleb Evans <caleb@calebevans.me>
2018-03-03 16:52:27 -0800

    Fix bare exception in setup file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_long_description():
         # Use pandoc to create reStructuredText README if possible
         import pypandoc
         return pypandoc.convert('README.md', 'rst')
-    except:
+    except Exception:
         return None
 
 
