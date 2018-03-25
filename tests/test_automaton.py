69738f5b8e9604b75ad49a72e86f322a7300e7c9
Caleb Evans <caleb@calebevans.me>
2018-03-24 20:06:01 -0700

    Add tests for abstract method NotImplementedError

---
+++ tests/test_automaton.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Functions for testing the Automaton abstract base class."""
+
+import nose.tools as nose
+
+from automata.base.automaton import Automaton
+
+
+def test_abstract_methods_not_implemented():
+    """Should raise NotImplementedError when calling abstract methods."""
+    with nose.assert_raises(NotImplementedError):
+        Automaton.__init__(Automaton)
+    with nose.assert_raises(NotImplementedError):
+        Automaton.validate_self(Automaton)
+    with nose.assert_raises(NotImplementedError):
+        Automaton._validate_input_yield(Automaton, None)
