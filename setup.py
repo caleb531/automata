cb7ee676341e7f04bd3cbd6fb801f1b80e856ec8
Caleb Evans <caleb@calebevans.me>
2018-05-28 19:42:04 -0700

    Prepare v2.1.0 release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_long_description():
 
 setup(
     name='automata-lib',
-    version='2.0.1',
+    version='2.1.0',
     description='A Python library for simulating automata and Turing machines',
     long_description=get_long_description(),
     url='https://github.com/caleb531/automata',
