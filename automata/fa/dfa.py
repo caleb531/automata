a9e9539a1883ea1b78378d02dcd15e620a6fdd59
Tagl <arnara14@ru.is>
2021-02-25 23:21:27 +0000

    DFA set operations

--- automata/fa/dfa.py
+++ automata/fa/dfa.py
@@ -24,6 +24,66 @@ class DFA(fa.FA):
         self.final_states = final_states.copy()
         self.validate()
 
+    def __eq__(self, other):
+        if isinstance(other, DFA):
+            sym_diff = self.symmetric_difference(other).minify()
+            return len(sym_diff.final_state) == 0
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, DFA):
+            return self.is_subset(other)
+        else:
+            raise NotImplementedError
+
+    def __ge__(self, other):
+        if isinstance(other, DFA):
+            return self.is_superset(other)
+        else:
+            raise NotImplementedError
+
+    def __lt__(self, other):
+        if isinstance(other, DFA):
+            return self <= other and self != other
+        else:
+            raise NotImplementedError
+
+    def __gt__(self, other):
+        if isinstance(other, DFA):
+            return self >= other and self != other
+        else:
+            raise NotImplementedError
+
+    def __sub__(self, other):
+        if isinstance(other, DFA):
+            return self.difference(other)
+        else:
+            raise NotImplementedError
+
+    def __or__(self, other):
+        if isinstance(other, DFA):
+            return self.union(other)
+        else:
+            raise NotImplementedError
+
+    def __and__(self, other):
+        if isinstance(other, DFA):
+            return self.intersection(other)
+        else:
+            raise NotImplementedError
+
+    def __xor__(self, other):
+        if isinstance(other, DFA):
+            return self.symmetric_difference(other)
+        else:
+            raise NotImplementedError
+
+    def __reversed__(self):
+        return self.reverse()
+
+    def __invert__(self):
+        return self.complement()
+
     def _validate_transition_missing_symbols(self, start_state, paths):
         """Raise an error if the transition input_symbols are missing."""
         for input_symbol in self.input_symbols:
@@ -223,6 +283,103 @@ class DFA(fa.FA):
         self.transitions = new_transitions
         self.initial_state = new_initial_state
         self.final_states = new_final_states
+
+    def _cross_product(self, other):
+        """
+        Creates a new DFA which is the cross product of DFAs self and other
+        with an empty set of final states.
+        """
+        assert self.input_symbols == other.input_symbols
+        states_a = list(self.states)
+        states_b = list(other.states)
+        new_states = {
+            self._stringify_states_unsorted((a, b))
+            for a in states_a for b in states_b
+        }
+        new_transitions = dict()
+        for state_a, transitions_a in self.transitions.items():
+            for state_b, transitions_b in other.transitions.items():
+                new_state = self._stringify_states_unsorted(
+                    (state_a, state_b)
+                )
+                new_transitions[new_state] = dict()
+                for symbol in self.input_symbols:
+                    new_transitions[new_state][symbol] = (
+                        self._stringify_states_unsorted(
+                            (transitions_a[symbol], transitions_b[symbol])
+                        )
+                    )
+        new_initial_state = self._stringify_states_unsorted(
+            (self.initial_state, other.initial_state)
+        )
+
+        return DFA(
+            states=new_states,
+            input_symbols=self.input_symbols,
+            transitions=new_transitions,
+            initial_state=new_initial_state,
+            final_states=set()
+        )
+
+    def union(self, other):
+        new_dfa = self._cross_product(other)
+        for state_a in self.states:
+            for state_b in other.states:
+                if (state_a in self.final_states or
+                        state_b in other.final_states):
+                    new_dfa.final_states.add(
+                        self._stringify_states_unsorted((state_a, state_b))
+                    )
+        new_dfa.validate()
+        return new_dfa
+
+    def intersect(self, other):
+        new_dfa = self._cross_product(other)
+        for state_a in self.final_states:
+            for state_b in other.final_states:
+                new_dfa.final_states.add(
+                    self._stringify_states_unsorted((state_a, state_b))
+                )
+        return new_dfa
+
+    def difference(self, other):
+        new_dfa = self._cross_product(other)
+        for state_a in self.final_states:
+            for state_b in other.states:
+                if state_b not in other.final_states:
+                    new_dfa.final_states.add(
+                        self._stringify_states_unsorted((state_a, state_b))
+                    )
+        return new_dfa
+
+    def symmetric_difference(self, other):
+        new_dfa = self._cross_product(other)
+        for state_a in self.states:
+            for state_b in other.states:
+                if ((state_a in self.final_states and
+                        state_b not in other.final_states) or
+                    (state_a not in self.final_states and
+                        state_b in other.final_states)):
+                    new_dfa.final_states.add(
+                        self._stringify_states_unsorted((state_a, state_b))
+                    )
+        return new_dfa
+
+    def complement(self):
+        new_dfa = self.copy()
+        new_dfa.final_states = self.states - self.final_states
+        return new_dfa
+
+    def is_subset(self, other):
+        return self.intersect(other).minify() == self
+
+    def is_superset(self, other):
+        return other.is_subset(self)
+
+    @staticmethod
+    def _stringify_states_unsorted(states):
+        """Stringify the given set of states as a single state name."""
+        return '{{{}}}'.format(','.join(states))
 
     @staticmethod
     def _stringify_states(states):
