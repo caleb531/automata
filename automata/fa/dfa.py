11b0c3697c9821fe6a37a57361998eb6a6ffa371
Caleb Evans <caleb@calebevans.me>
2022-08-16 13:12:18 -0700

    Use generator expression to stringify DFA state names
    For better performance compared to list comprehension.

--- automata/fa/dfa.py
+++ automata/fa/dfa.py
@@ -513,7 +513,7 @@ class DFA(fa.FA):
     @staticmethod
     def _stringify_states(states):
         """Stringify the given set of states as a single state name."""
-        return '{{{}}}'.format(','.join(sorted([str(state) for state in states])))
+        return '{{{}}}'.format(','.join(sorted(str(state) for state in states)))
 
     @classmethod
     def _add_nfa_states_from_queue(cls, nfa, current_states,
