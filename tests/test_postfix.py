8a13c8739e065feac80128cd20058f188d85ab73
Eliot Robson <eliot.robson24@gmail.com>
2022-10-07 18:06:00 -0500

    Add postfix tests

---
+++ tests/test_postfix.py
@@ -0,0 +1,143 @@
+"""Tests of the postfix conversion and parsing utility functions."""
+
+import unittest
+import automata.base.exceptions as exceptions
+import automata.regex.postfix as postfix
+from automata.regex.lexer import Lexer, Token
+from typing import List
+
+
+class Integer(postfix.Literal):
+    def val(self) -> int:
+        """It evaluates to its (integer) value."""
+        return int(self.text)
+
+class Add(postfix.InfixOperator):
+    """Addition."""
+    def get_precedence(self) -> int:
+        return 10  # Precedence: higher than integers, lower than mult
+
+    def op(self, left: int, right: int) -> int:
+        return left + right
+
+class Minus(postfix.InfixOperator):
+    """Subtraction."""
+    def get_precedence(self) -> int:
+        return 10  # Precedence: higher than integers, lower than mult
+
+    def op(self, left: int, right: int) -> int:
+        return left - right
+
+
+class Mult(postfix.InfixOperator):
+    """Multiplication."""
+    def get_precedence(self) -> int:
+        return 20  # Higher precedence than addition/substraction.
+
+    def op(self, left: int, right: int) -> int:
+        return left * right
+
+class Divide(postfix.InfixOperator):
+    """Division."""
+    def get_precedence(self) -> int:
+        return 20  # Same precedence than multiplication
+
+    def op(self, left: int, right: int) -> int:
+        return left // right
+
+
+def test_nested_parenthesized_expression() -> None:
+    # Parsing:
+    # "( 4 + ( 1 + 2 * 3 * ( 4 + 5 ) + 6 ) ) * 7 + 8"
+    tokens: List[Token] = [
+        postfix.LeftParen('('),
+        Integer('4'),
+        Add('+'),
+        postfix.LeftParen('('),
+        Integer('1'),
+        Add('+'),
+        Integer('2'),
+        Mult('*'),
+        Integer('3'),
+        Mult('*'),
+        postfix.LeftParen('('),
+        Integer('4'),
+        Add('+'),
+        Integer('5'),
+        postfix.RightParen(')'),
+        Add('+'),
+        Integer('6'),
+        postfix.RightParen(')'),
+        postfix.RightParen(')'),
+        Mult('*'),
+        Integer('7'),
+        Add('+'),
+        Integer('8'),
+    ]
+
+    postfix_tokens: List[Token] = postfix.tokens_to_postfix(tokens)
+    res = postfix.parse_postfix_tokens(postfix_tokens)
+    assert (4 + (1 + 2 * 3 * (4 + 5) + 6)) * 7 + 8 == res
+
+class TestArithmeticParser(unittest.TestCase):
+    """Test parsing arithmetic expressions."""
+
+    def setUp(self):
+        self.arithmetic_lexer: Lexer = Lexer()
+
+        self.arithmetic_lexer.register_token(lambda x: postfix.LeftParen(x), r'\(')
+        self.arithmetic_lexer.register_token(lambda x: postfix.RightParen(x), r'\)')
+        self.arithmetic_lexer.register_token(lambda x: Integer(x), r'[0-9]+')
+        self.arithmetic_lexer.register_token(lambda x: Add(x), r'\+')
+        self.arithmetic_lexer.register_token(lambda x: Minus(x), r'-')
+        self.arithmetic_lexer.register_token(lambda x: Mult(x), r'\*')
+        self.arithmetic_lexer.register_token(lambda x: Divide(x), r'/')
+
+    def assert_invalid_ordering(self, regexp):
+        with self.assertRaises(exceptions.InvalidRegexError):
+            postfix.validate_tokens(self.arithmetic_lexer.lex(regexp))
+
+    def test_expression_invalid_ordering(self):
+        statements = ['+6', '+5+', '6/', '1 + 2 - + 3', ')(', '(((2))', '(+5', '6/)']
+
+        for statement in statements:
+            self.assert_invalid_ordering(statement)
+
+    def parse(self, tokens: List[Token]) -> int:
+        postfix_tokens = postfix.tokens_to_postfix(tokens)
+        return postfix.parse_postfix_tokens(postfix_tokens)
+
+    def test_single_number(self) -> None:
+        val = self.parse(self.arithmetic_lexer.lex('13'))
+        assert 13 == val
+
+    def test_negative_number(self) -> None:
+        val = self.parse(self.arithmetic_lexer.lex('0-13'))
+        assert -13 == val
+
+    def test_simple_mult(self) -> None:
+        assert 8 == self.parse(self.arithmetic_lexer.lex('2 * 4'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('2 * 2 * 2'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('2 * (2 * 2)'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('(2 * 2) * 2'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('(2 + 2) * 2'))
+
+    def test_precedence(self) -> None:
+        assert 8 == self.parse(self.arithmetic_lexer.lex('2 * 3 + 2'))
+        assert 10 == self.parse(self.arithmetic_lexer.lex('2 * (3+2)'))
+
+    def test_negative_mult(self) -> None:
+        assert 8 == self.parse(self.arithmetic_lexer.lex('(0-2) * (0- 4)'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('(0-2) * 2 * (0-2)'))
+        assert -5 == self.parse(self.arithmetic_lexer.lex('1 + (0-2) * 3'))
+
+    def test_division(self) -> None:
+        assert 2 == self.parse(self.arithmetic_lexer.lex('4 / 2'))
+        assert 2 == self.parse(self.arithmetic_lexer.lex('5 / 2'))
+        assert 2 == self.parse(self.arithmetic_lexer.lex('6 - 8 / 2'))
+        assert 3 == self.parse(self.arithmetic_lexer.lex('3 * 2 / 2'))
+        assert 3 == self.parse(self.arithmetic_lexer.lex('2 * 3 / 2'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('8 / 2 * 2'))
+        assert 8 == self.parse(self.arithmetic_lexer.lex('(8 / 2) * 2'))
+        assert 2 == self.parse(self.arithmetic_lexer.lex('8 / (2 * 2)'))
+        assert 2 == self.parse(self.arithmetic_lexer.lex('16/4/2'))
