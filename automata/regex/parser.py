96853a1c3c7c296cbdbe09a7edc0a2af257a06a5
Eliot Robson <eliot.robson24@gmail.com>
2022-10-16 12:38:46 -0500

    Remove concat token from parser and fix old comment

--- automata/regex/parser.py
+++ automata/regex/parser.py
@@ -202,7 +202,6 @@ def get_regex_lexer():
     lexer.register_token(RightParen, r'\)')
     lexer.register_token(StringToken, r'[A-Za-z0-9]')
     lexer.register_token(UnionToken, r'\|')
-    lexer.register_token(ConcatToken, r'\.')
     lexer.register_token(KleeneToken, r'\*')
     lexer.register_token(OptionToken, r'\?')
 
