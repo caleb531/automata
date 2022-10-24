77f52a4b867786e0e5c1f2fd0260383d73301599
Eliot Robson <eliot.robson24@gmail.com>
2022-10-24 12:07:57 -0500

    Removed redundant minify calls

--- automata/regex/regex.py
+++ automata/regex/regex.py
@@ -40,8 +40,8 @@ def issubset(re1, re2):
 
     nfa1 = NFA.from_regex(re1)
     nfa2 = NFA.from_regex(re2)
-    dfa1 = DFA.from_nfa(nfa1).minify(retain_names=False)
-    dfa2 = DFA.from_nfa(nfa2).minify(retain_names=False)
+    dfa1 = DFA.from_nfa(nfa1)
+    dfa2 = DFA.from_nfa(nfa2)
 
     return dfa1.issubset(dfa2)
 
@@ -51,7 +51,7 @@ def issuperset(re1, re2):
 
     nfa1 = NFA.from_regex(re1)
     nfa2 = NFA.from_regex(re2)
-    dfa1 = DFA.from_nfa(nfa1).minify(retain_names=False)
-    dfa2 = DFA.from_nfa(nfa2).minify(retain_names=False)
+    dfa1 = DFA.from_nfa(nfa1)
+    dfa2 = DFA.from_nfa(nfa2)
 
     return dfa2.issubset(dfa1)
