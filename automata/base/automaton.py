7f14e0cfd0562c8c0c3dd8526f64f7b4aa9d3fae
Eliot Robson <eliot.robson24@gmail.com>
2023-03-28 19:02:36 -0500

    Started annotating

--- automata/base/automaton.py
+++ automata/base/automaton.py
@@ -8,12 +8,23 @@ from frozendict import frozendict
 import automata.base.config as global_config
 import automata.base.exceptions as exceptions
 from automata.base.utils import freeze_value
+from typing import Any, NoReturn, Dict, Tuple, Union
 
+FAStateT = Any
+PathT = frozendict[str, Any]
+TransitionT = frozendict[str, PathT]
 
 class Automaton(metaclass=abc.ABCMeta):
     """An abstract base class for all automata, including Turing machines."""
 
-    def __init__(self, **kwargs):
+    __slots__ : Tuple[str, ...] = tuple()
+
+    initial_state: FAStateT
+    states: Union[frozenset, set]
+    final_states: Union[frozenset, set]
+    transitions: TransitionT
+
+    def __init__(self, **kwargs: Any) -> None:
         if not global_config.allow_mutable_automata:
             for attr_name, attr_value in kwargs.items():
                 object.__setattr__(self, attr_name, freeze_value(attr_value))
@@ -22,39 +33,40 @@ class Automaton(metaclass=abc.ABCMeta):
                 object.__setattr__(self, attr_name, attr_value)
         self.__post_init__()
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if global_config.should_validate_automata:
             self.validate()
 
     @abc.abstractmethod
-    def validate(self):
+    def validate(self) -> None:
         """Return True if this automaton is internally consistent."""
         raise NotImplementedError
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, name: str, value: Any) -> NoReturn:
         """Set custom setattr to make class immutable."""
         raise AttributeError(f'This {type(self).__name__} is immutable')
 
-    def __delattr__(self, name):
+    def __delattr__(self, name: str) -> None:
         """Set custom delattr to make class immutable."""
         raise AttributeError(f'This {type(self).__name__} is immutable')
 
-    def __getstate__(self):
+    def __getstate__(self) -> Any:
         """Return the object's state, described by its input parameters"""
         return self.input_parameters
 
-    def __setstate__(self, d):
+    def __setstate__(self, d: Dict[str, Any]) -> None:
         """Restore the object state from its input parameters"""
         # Notice that the default __setstate__ method won't work
         #   because __setattr__ is disabled due to immutability
-        self.__init__(**d)
+        self.__init__(**d) # type: ignore
 
-    @abc.abstractmethod
-    def read_input_stepwise(self, input_str):
+    #TODO add annotation for generator here
+    @abc.abstractmethod 
+    def read_input_stepwise(self, input_str: str):
         """Return a generator that yields each step while reading input."""
         raise NotImplementedError
 
-    def read_input(self, input_str):
+    def read_input(self, input_str: str) -> FAStateT:
         """
         Check if the given string is accepted by this automaton.
 
@@ -65,7 +77,7 @@ class Automaton(metaclass=abc.ABCMeta):
             pass
         return config
 
-    def accepts_input(self, input_str):
+    def accepts_input(self, input_str: str) -> bool:
         """Return True if this automaton accepts the given input."""
         try:
             self.read_input(input_str)
@@ -73,20 +85,20 @@ class Automaton(metaclass=abc.ABCMeta):
         except exceptions.RejectionException:
             return False
 
-    def _validate_initial_state(self):
+    def _validate_initial_state(self) -> None:
         """Raise an error if the initial state is invalid."""
         if self.initial_state not in self.states:
             raise exceptions.InvalidStateError(
                 '{} is not a valid initial state'.format(self.initial_state))
 
-    def _validate_initial_state_transitions(self):
+    def _validate_initial_state_transitions(self) -> None:
         """Raise an error if the initial state has no transitions defined."""
         if self.initial_state not in self.transitions and len(self.states) > 1:
             raise exceptions.MissingStateError(
                 'initial state {} has no transitions defined'.format(
                     self.initial_state))
 
-    def _validate_final_states(self):
+    def _validate_final_states(self) -> None:
         """Raise an error if any final states are invalid."""
         invalid_states = self.final_states - self.states
         if invalid_states:
@@ -95,19 +107,20 @@ class Automaton(metaclass=abc.ABCMeta):
                     ', '.join(str(state) for state in invalid_states)))
 
     @property
-    def input_parameters(self):
+    def input_parameters(self) -> Dict[str, Any]:
         """Return the public attributes for this automaton."""
         return {attr_name: getattr(self, attr_name)
                 for attr_name in self.__slots__
                 if not attr_name.startswith('_')}
 
-    def copy(self):
+    #TODO add annotation to here?
+    def copy(self):# -> Self:
         """Create a deep copy of the automaton."""
         return self.__class__(**self.input_parameters)
 
     # Format the given value for string output via repr() or str(); this exists for the purpose of displaying
 
-    def _get_repr_friendly_value(self, value):
+    def _get_repr_friendly_value(self, value: Any) -> Any:
         """
         A helper function to convert the given value / structure into a fully
         mutable one by recursively processing said structure and any of its
@@ -124,13 +137,13 @@ class Automaton(metaclass=abc.ABCMeta):
         else:
             return value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Return a string representation of the automaton."""
         values = ', '.join(
             f'{attr_name}={self._get_repr_friendly_value(attr_value)!r}'
             for attr_name, attr_value in self.input_parameters.items())
         return f'{self.__class__.__qualname__}({values})'
 
-    def __contains__(self, input_str):
+    def __contains__(self, input_str: str) -> bool:
         """Returns whether the word is accepted by the automaton."""
         return self.accepts_input(input_str)
