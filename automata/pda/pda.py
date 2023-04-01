d7b37d9cf7c0924e6c7635895476a597768e4989
Caleb Evans <caleb@calebevans.me>
2023-04-01 11:12:53 -0700

    Add types to abstract PDA class

--- automata/pda/pda.py
+++ automata/pda/pda.py
@@ -2,46 +2,56 @@
 """Classes and methods for working with all pushdown automata."""
 
 import abc
+from typing import Union
+
+from typing_extensions import Literal
 
 import automata.base.exceptions as exceptions
 import automata.pda.exceptions as pda_exceptions
-from automata.base.automaton import Automaton
+from automata.base.automaton import Automaton, AutomatonStateT
+from automata.pda.configuration import PDAConfiguration
+from automata.pda.stack import PDAStack
+
+PDAStateT = AutomatonStateT
+PDAAcceptanceModeT = Union[Literal['final_state'],
+                           Literal['empty_stack'],
+                           Literal['both']]
 
 
 class PDA(Automaton, metaclass=abc.ABCMeta):
     """An abstract base class for pushdown automata."""
 
-    def _validate_transition_invalid_input_symbols(self, start_state,
-                                                   input_symbol):
+    def _validate_transition_invalid_input_symbols(self, start_state: PDAStateT,
+                                                   input_symbol: str) -> None:
         """Raise an error if transition input symbols are invalid."""
         if input_symbol not in self.input_symbols and input_symbol != '':
             raise exceptions.InvalidSymbolError(
                 'state {} has invalid transition input symbol {}'.format(
                     start_state, input_symbol))
 
-    def _validate_transition_invalid_stack_symbols(self, start_state,
-                                                   stack_symbol):
+    def _validate_transition_invalid_stack_symbols(self, start_state: PDAStateT,
+                                                   stack_symbol: str) -> None:
         """Raise an error if transition stack symbols are invalid."""
         if stack_symbol not in self.stack_symbols:
             raise exceptions.InvalidSymbolError(
                 'state {} has invalid transition stack symbol {}'.format(
                     start_state, stack_symbol))
 
-    def _validate_initial_stack_symbol(self):
+    def _validate_initial_stack_symbol(self) -> None:
         """Raise an error if initial stack symbol is invalid."""
         if self.initial_stack_symbol not in self.stack_symbols:
             raise exceptions.InvalidSymbolError(
                 'initial stack symbol {} is invalid'.format(
                     self.initial_stack_symbol))
 
-    def _validate_acceptance(self):
+    def _validate_acceptance(self) -> None:
         """Raise an error if the acceptance mode is invalid."""
         if self.acceptance_mode not in ('final_state', 'empty_stack', 'both'):
             raise pda_exceptions.InvalidAcceptanceModeError(
                 'acceptance mode {} is invalid'.format(
                     self.acceptance_mode))
 
-    def validate(self):
+    def validate(self) -> bool:
         """Return True if this PDA is internally consistent."""
         for start_state, paths in self.transitions.items():
             self._validate_transition_invalid_symbols(start_state, paths)
@@ -51,13 +61,13 @@ class PDA(Automaton, metaclass=abc.ABCMeta):
         self._validate_acceptance()
         return True
 
-    def _has_lambda_transition(self, state, stack_symbol):
+    def _has_lambda_transition(self, state: PDAStateT, stack_symbol: str) -> bool:
         """Return True if the current config has any lambda transitions."""
         return (state in self.transitions and
                 '' in self.transitions[state] and
                 stack_symbol in self.transitions[state][''])
 
-    def _replace_stack_top(self, stack, new_stack_top):
+    def _replace_stack_top(self, stack: PDAStack, new_stack_top: str) -> PDAStack:
         """Replace the top of the PDA stack with another symbol"""
         if new_stack_top == '':
             new_stack = stack.pop()
@@ -65,7 +75,7 @@ class PDA(Automaton, metaclass=abc.ABCMeta):
             new_stack = stack.replace(new_stack_top)
         return new_stack
 
-    def _has_accepted(self, current_configuration):
+    def _has_accepted(self, current_configuration: PDAConfiguration) -> bool:
         """Check whether the given config indicates accepted input."""
         # If there's input left, we're not finished.
         if current_configuration.remaining_input:
