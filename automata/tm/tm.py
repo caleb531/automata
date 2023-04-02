fe6f9c88658eaa22a9d4e0cb89df749b494ec0ed
Caleb Evans <caleb@calebevans.me>
2023-04-01 17:47:09 -0700

    Add types for TM abstract base class

--- automata/tm/tm.py
+++ automata/tm/tm.py
@@ -2,29 +2,41 @@
 """Classes and methods for working with all Turing machines."""
 
 import abc
+from typing import AbstractSet, Union
+
+from typing_extensions import Literal
 
 import automata.base.exceptions as exceptions
-from automata.base.automaton import Automaton
+from automata.base.automaton import Automaton, AutomatonStateT, AutomatonTransitionsT
+
+TMStateT = AutomatonStateT
+PDATransitionsT = AutomatonTransitionsT
+PDAAcceptanceModeT = Union[
+    Literal["final_state"], Literal["empty_stack"], Literal["both"]
+]
 
 
 class TM(Automaton, metaclass=abc.ABCMeta):
     """An abstract base class for Turing machines."""
 
-    def _read_input_symbol_subset(self):
+    tape_symbols: AbstractSet[str]
+    blank_symbol: str
+
+    def _read_input_symbol_subset(self) -> None:
         if not (self.input_symbols < self.tape_symbols):
             raise exceptions.MissingSymbolError(
                 "The set of tape symbols is missing symbols from the input "
                 "symbol set ({})".format(self.tape_symbols - self.input_symbols)
             )
 
-    def _validate_blank_symbol(self):
+    def _validate_blank_symbol(self) -> None:
         """Raise an error if blank symbol is not a tape symbol."""
         if self.blank_symbol not in self.tape_symbols:
             raise exceptions.InvalidSymbolError(
                 "blank symbol {} is not a tape symbol".format(self.blank_symbol)
             )
 
-    def _validate_nonfinal_initial_state(self):
+    def _validate_nonfinal_initial_state(self) -> None:
         """Raise an error if the initial state is a final state."""
         if self.initial_state in self.final_states:
             raise exceptions.InitialStateError(
