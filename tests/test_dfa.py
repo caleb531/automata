5ccaaf1e76c79edae11e64810af515bc54570b06
Arnar Bjarni Arnarson <arnara14@ru.is>
2024-03-03 22:37:17 +0000

    Aho-Corasick (#167)
    * Aho-Corasick for multiple substring construction
    
    * Add simple tests for Aho-Corasick
    
    * Type annotations
    
    * Fix type hints
    
    * Fix type hints
    
    * Update test_dfa.py
    
    * Update dfa.py
    
    * Updated docstring
    
    * Update dfa.py
    
    * Update dfa.py
    
    * Add annotation
    
    * Reformat docstring
    
    * Update test_dfa.py
    
    ---------
    
    Co-authored-by: Eliot Robson <eliot.robson24@gmail.com>

--- tests/test_dfa.py
+++ tests/test_dfa.py
@@ -2133,6 +2133,39 @@ class TestDFA(test_fa.TestFA):
                 break
             self.assertIn("nano", word)
 
+    def test_contains_substrings(self) -> None:
+        input_symbols = {"a", "n", "o", "b"}
+        substring_dfa = DFA.from_substring(input_symbols, "nano")
+        substrings_dfa = DFA.from_substrings(input_symbols, {"nano"})
+
+        self.assertEqual(substring_dfa, substrings_dfa)
+
+        substring_dfa = substring_dfa | DFA.from_substring(input_symbols, "banana")
+        substrings_dfa = DFA.from_substrings(input_symbols, {"banana", "nano"})
+
+        self.assertEqual(substring_dfa, substrings_dfa)
+
+        self.assertEqual(
+            ~substrings_dfa,
+            DFA.from_substrings(input_symbols, {"banana", "nano"}, contains=False),
+        )
+
+        m = 50
+        n = 50
+        input_symbols = {"a", "b"}
+        language = {("a" * i + "b" * j) for i, j in product(range(n), range(m))}
+
+        equiv_dfa = DFA.from_substrings(
+            input_symbols,
+            language,
+        )
+
+        res_dfa = DFA.empty_language(input_symbols)
+        for string in language:
+            res_dfa |= DFA.from_substring(input_symbols, string)
+
+        self.assertEqual(equiv_dfa, res_dfa)
+
     @params(True, False)
     def test_contains_subsequence(self, as_partial: bool) -> None:
         """Should compute the minimal DFA accepting strings with the given
